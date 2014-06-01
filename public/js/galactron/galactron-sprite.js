4ed4caada9a281aa49d6d889b7819830f71242ea
Garcia Hurtado <ghurtado@gmail.com>
2014-05-31 23:07:39 -0400

    Added physics shortcut functions to GalactronSprite.

--- public/js/galactron/galactron-sprite.js
+++ public/js/galactron/galactron-sprite.js
@@ -1,10 +1,14 @@
 class GalactronSprite extends Phaser.Sprite {
 	constructor(game, x = 0, y = 0, graphic) {
+		this.math = game.math; // convenience
+
 		super(game, x, y, graphic);
 		this.actions = new ActionChain(game, this);
 		game.add.existing(this.actions);		
 
 		this.flickering = false;
+
+		this.game.physics.enable(this, Phaser.Physics.ARCADE);
 	}
 
 	/**
@@ -31,18 +35,6 @@ class GalactronSprite extends Phaser.Sprite {
 		}
 	}
 
-	/**
-	 * Changes the position of this sprite, such that its center is placed exactly at the center of the
-	 * provided sprite
-	 */
-	centerAt(target) {
-		this.x = target.x + Math.round(target.width * .5);
-		this.y = target.y + Math.round(target.height * .5);
-
-		// substract half the width and height of this sprite from x,y to finish centering it
-		this.x -= Math.round(this.width / 2);
-		this.y -= Math.round(this.height / 2);
-	}
 
 	/**
 	 * Refactor: combine with init?
@@ -95,5 +87,36 @@ class GalactronSprite extends Phaser.Sprite {
 	 */
 	deathAnimation() {
 		return;
+	}
+
+	/* ------------- PHYSICS --------------------*/
+
+	/**
+	 * Calculate the angle of the vector between this sprite and a target, in radians
+	 * 
+	 * @param	target
+	 */
+	angleTo(target) {
+		return this.math.angleBetweenPoints(this, target);
+	}
+
+	/**
+	 * Return the distance in pixels between this sprite and any other target with x,y coordinates
+	 */
+	distanceTo(target){
+		return this.math.distance(this.x, this.y, target.x, target.y);
+	}
+
+	/**
+	 * Moves this sprite such that its center is placed exactly at the center of the
+	 * provided sprite
+	 */
+	centerAt(target) {
+		this.x = target.x + Math.round(target.width * .5);
+		this.y = target.y + Math.round(target.height * .5);
+
+		// substract half the width and height of this sprite from x,y to finish centering it
+		this.x -= Math.round(this.width / 2);
+		this.y -= Math.round(this.height / 2);
 	}
 }
